1183813e6b2e9926d1d0f4d9741bb0b575b0796b
Santosharawn7 <santosharawn7@gmail.com>
2025-07-20 14:33:45 -0400

    axios fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,14 @@
+// main.jsx
+import './axiosConfig'; // <-- This line must be FIRST!
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-// 1) Import once at root:
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {/* 2) Render your ToastContainer here */}
     <ToastContainer
       position="top-center"
       autoClose={3000}
